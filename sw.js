788939768d14b0a099b27c25aa036b9321c7d2c9
Frankson Ayiakwo <frankchord56@gmail.com>
2026-06-11 23:28:07 +0000

    converted into mobile app

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'coreattend-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  './itds.png',
+  './uenr.png',
+  './student_enter_code/Student-eneter code.html',
+  './student_your_details/Students-details.html',
+  './success_attendance_recorded/Success.html',
+  './lecturer_dashboard/lecturer-dashboard.html',
+  './stitch/student_your_details/code.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch new
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
